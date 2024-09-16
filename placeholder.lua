b4fc011a3051d4aa26e2835f65cd9777a5e3cff6
vortexx <108076105+agreeed@users.noreply.github.com>
2024-09-16 13:25:13 +0500

    Update placeholder.lua

--- placeholder.lua
+++ placeholder.lua
@@ -1,6 +1,8 @@
 -- If executor closure is not present, replaces it with a placeholder function/table/etc.
 local function replaceFunc(cloj, clst, ret)
   if cloj then return end
+  print(cloj,clst,ret)
+  print("replacing",clst,"with function returning",typeof(ret))
   getgenv()[clst] = function() return ret end
 end
 
